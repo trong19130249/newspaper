9025ca1928da979a65cf413aca3387d98aa6d5f3
TrongNgo <trongtan252001@gmail.com>
2022-07-20 21:49:34 +0700

    add component header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -46,9 +46,11 @@ import { MdbTransferModule } from 'mdb-angular-transfer';
 import { MdbMentionModule } from 'mdb-angular-mention';
 import { MdbCookiesManagementService } from 'mdb-angular-cookies-management';
 import { MdbStorageManagementService } from 'mdb-angular-storage-management';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
