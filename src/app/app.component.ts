bc8ba0b2e19c0a7db3751afd9ef8b410636ec223
TrongNgo <trongtan252001@gmail.com>
2022-08-19 17:04:32 +0700

    add:lib xml2js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import * as converter from 'xml-js';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Tiền phong';
+  title = 'Tiền phong'
+  list: any = [];
+  page:string ="home";
+  constructor(private http:HttpClient) {
+    this.loadXML();
+  }
+  //getting data function
+  loadXML()
+  {
+    /*Read Data*/
+    this.http.get('https://tienphong.vn/rss/'+this.page+".rss",
+      { responseType: 'text' })
+      .subscribe((data) => {
+        let result1 = converter.xml2json(data, { compact: true, spaces: 2 });
+        console.log(result1);
+
+        let JSONData:any = JSON.parse(result1);
+        console.log(JSONData);
+        this.list = [];
+
+      });
+    /*Read Data*/
+  }
+
 }
