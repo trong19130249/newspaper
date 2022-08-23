d5e1f993cd57a06c707a64586ff67d8befb7f106
Tuan <19130257@st.hcmuaf.edu.vn>
2022-08-23 22:13:22 +0700

    fill data into slidebar

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SocialWidgetService } from '../share/social-widget.service';
+import {RssLinkService} from "../share/rss-link.service";
+import {FeedService} from "../share/feed.service";
+import {Item} from "../model/Item";
 
 @Component({
   selector: 'app-sidebar',
@@ -7,10 +10,18 @@ import { SocialWidgetService } from '../share/social-widget.service';
   styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit {
+  navItems:any;
   dataSocial: any;
-  constructor(private socialService: SocialWidgetService) {
+  listItems: Item[] = [];
+  constructor(private socialService: SocialWidgetService,  private rssLinkService: RssLinkService, private feedService: FeedService) {
     this.dataSocial = this.socialService.SOCIAL_DATA;
+
   }
 
-  ngOnInit(): void {}
+  async ngOnInit(): Promise<any> {
+    this.navItems =this.rssLinkService.LIST_RSS_HOME;
+    const data = await this.feedService.getDataHome();
+    const channel = data?.rss?.channel[0];
+    this.listItems = channel.item;
+  }
 }
